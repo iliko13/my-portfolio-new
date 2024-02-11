31fe1be5163d484bfaa39a929da7fdf606f61f17
iliko13 <107374131+iliko13@users.noreply.github.com>
2024-02-11 19:53:19 +0400

    sixth commit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,19 +1,31 @@
 "use client";
 
-import React from "react";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
 import SectionHeading from "./SectionHeading";
 import { FaPaperPlane } from "react-icons/fa";
-import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
-function Contact() {
+const Contact = () => {
   const { ref } = useSectionInView("Contact");
 
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
   return (
     <motion.section
       ref={ref}
       id="contact"
-      className="mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center"
+      className="mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center scroll-mt-28"
       initial={{
         opacity: 0,
       }}
@@ -36,31 +48,68 @@ function Contact() {
         or through this form.
       </p>
 
-      <form className="mt-10 flex flex-col">
+      <form
+        target="_blank"
+        onSubmit={onSubmit}
+        method="Post"
+        action="https://formsubmit.co/70086aaaa0af3abaa037776c910d6b42"
+        className="mt-10 flex flex-col"
+      >
         <input
           className="h-14 px-4 rounded-lg border border-black/10"
-          type="email"
-          required
-          maxLength={500}
-          placeholder="Your Email"
+          type="text"
+          placeholder="NAME"
+          {...register("name", { required: true, maxLength: 20 })}
         />
+        {errors.name && (
+          <p className="paragraphFont mt-1 select-none text-red-400">
+            {errors.name.type === "required" && "This filed is required."}
+            {errors.name.type === "maxLength" && "Max length is 20 character."}
+          </p>
+        )}
+
+        <input
+          className="h-14 px-4 rounded-lg border border-black/10 mt-3"
+          type="text"
+          placeholder="EMAIL"
+          {...register("email", {
+            required: true,
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          })}
+        />
+        {errors.email && (
+          <p className="paragraphFont mt-1 select-none text-red-400">
+            {errors.email.type === "required" && "This filed is required."}
+            {errors.email.type === "pattern" && "Invalid email address"}
+          </p>
+        )}
+
         <textarea
           className="h-52 my-3 rounded-lg border border-black/10 p-4"
-          placeholder="Your message"
-          required
-          maxLength={500}
+          rows={4}
+          cols={50}
+          placeholder="MESSAGE"
+          {...register("message", { required: true, maxLength: 2000 })}
         />
+        {errors.message && (
+          <p className="paragraphFont mt-1 select-none text-red-400">
+            {errors.message.type === "required" && "This filed is required."}
+            {errors.message.type === "maxLength" &&
+              "Max length is 2000 character."}
+          </p>
+        )}
+
         <button
           type="submit"
           className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white 
           rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
         >
-          Submit{" "}
+          Submit
           <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
     </motion.section>
   );
-}
+};
 
 export default Contact;
